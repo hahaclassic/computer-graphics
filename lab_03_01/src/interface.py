aa311e6fff03b2e18578101a8b8fe673c0c978fa
hahaclassic <gavrilyukvlad221@gmail.com>
2024-04-04 13:36:03 +0300

    added interface

---
+++ lab_03_01/src/interface.py
@@ -0,0 +1,70 @@
+from PyQt6.QtWidgets import QMainWindow, QMenu, QMenuBar, QFileDialog, QMessageBox, \
+      QPushButton, QColorDialog, QLabel, QGraphicsView, QGraphicsScene, QTextEdit, QComboBox
+from PyQt6.QtGui import QAction, QColor, QTransform
+from PyQt6.QtCore import QPointF, QLineF
+from PyQt6 import uic
+
+class Interface(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("ui/mainwindow.ui", self)
+        self.scene = QGraphicsScene()
+        self.view = self.findChild(QGraphicsView, "graphicsView")
+        self.view.setScene(self.scene)
+        transform = QTransform()
+        transform.scale(1, -1)
+        self.view.setTransform(transform)
+
+        self.background_color_button = self.findChild(QPushButton, "changeBackgroundColorBtn")
+        self.background_color_button.clicked.connect(self.choose_background_color)
+        self.background_color_label = self.findChild(QLabel, "backgroundColor")
+        self.background_color = QColor(255, 255, 255)
+
+        self.segment_color_button = self.findChild(QPushButton, "changeSegColorBtn")
+        self.segment_color_button.clicked.connect(self.choose_segment_color)
+        self.segment_color_label = self.findChild(QLabel, "segmentColor")
+        self.segment_color = QColor(0, 0, 0)
+
+        self.input_start_x = self.findChild(QTextEdit, "startCoordX")
+        self.input_start_y = self.findChild(QTextEdit, "startCoordY")
+        self.input_end_x = self.findChild(QTextEdit, "endCoordX")
+        self.input_end_y = self.findChild(QTextEdit, "endCoordY")
+
+        self.algorithm = self.findChild(QComboBox, "AlgorithmTypeBox")
+        
+        self.plot_segment_button = self.findChild(QPushButton, "plotSegmentBtn")
+        self.plot_segment_button.clicked.connect(self.plot_segment)
+
+    def choose_background_color(self):
+        color_dialog = QColorDialog(self)
+        if color_dialog.exec():
+            self.background_color = color_dialog.currentColor()
+            self.background_color_label.setStyleSheet(
+                f"background-color: {self.background_color.name()}"
+            )
+            self.view.setBackgroundBrush(self.background_color)
+        
+    def choose_segment_color(self):
+        color_dialog = QColorDialog(self)
+        if color_dialog.exec():
+            self.segment_color = color_dialog.currentColor()
+            self.segment_color_label.setStyleSheet(
+                f"background-color: {self.segment_color.name()}"
+            )
+
+    def plot_segment(self):
+        try:
+            start_x = float(self.input_start_x.toPlainText())
+            start_y = float(self.input_start_y.toPlainText())
+            end_x = float(self.input_end_x.toPlainText())
+            end_y = float(self.input_end_y.toPlainText())
+        except ValueError:
+            return
+
+        start, end = QPointF(start_x, start_y), QPointF(end_x, end_y)
+
+        match self.algorithm.currentIndex():
+            case 5: # "Алгоритм, использующий библиотечную функцию":
+                line = QLineF(start, end)
+                self.scene.addLine(line, self.segment_color)
+                
