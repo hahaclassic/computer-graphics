9537af00399e0de687cd806897f4215c80ab431c
hahaclassic <gavrilyukvlad221@gmail.com>
2024-04-06 22:34:42 +0300

    v.1.0

--- lab_03_01/src/geometry.py
+++ lab_03_01/src/geometry.py
@@ -1,111 +1,25 @@
 from PyQt6.QtCore import QPointF
-from PyQt6.QtWidgets import QGraphicsScene, QGraphicsEllipseItem
-from PyQt6.QtGui import QColor
-
+from PyQt6.QtGui import QVector2D
 import math
 
-def plot_point(scene: QGraphicsScene, color: QColor, point: QPointF):
-    scene.addEllipse(point.x(), point.y(), 0.5, 0.5, color)
+def multiply_vector_by_matrix(vector: QVector2D,
+                              matrix: list[list[float]]) -> QVector2D:
+
+    x = matrix[0][0] * vector.x() + matrix[0][1] * vector.y()
+    y = matrix[1][0] * vector.x() + matrix[1][1] * vector.y()
+
+    return QVector2D(x, y)
+
+def rotate_vector(vector: QVector2D, angle: float) -> QVector2D:
+    rotate_matrix = [
+        [math.cos(angle), -math.sin(angle)],
+        [math.sin(angle), math.cos(angle)]
+    ]
+    return multiply_vector_by_matrix(vector, rotate_matrix)
 
 
-def digital_differential_analyzer(scene: QGraphicsScene, color: QColor, start: QPointF, end: QPointF):
-    if start == end:
-        plot_point(start)
-        return
-    
-    diff: QPointF = end - start
-    diff_x = math.fabs(diff.x()) 
-    diff_y = math.fabs(diff.y())
-    if diff_x > diff_y:
-        l = diff_x
-    else:
-        l = diff_y
+def rotate_point(point: QPointF, center: QPointF, angle: float) -> QPointF:
+    vector = QVector2D(point - center)
+    rotated_vec = rotate_vector(vector, angle)
 
-    dx = diff.x() / l
-    dy = diff.y() / l
-
-    curr_x, curr_y = start.x(), start.y()
-    for _ in range(1, int(l) + 1):
-        x, y = round(curr_x), round(curr_y)
-        plot_point(scene, color, QPointF(x, y))
-        curr_x += dx
-        curr_y += dy
-
-
-def sign(num: float) -> int:
-    if num > 0:
-        return 1
-    elif num < 0:
-        return -1
-
-    return 0
-
-
-def float_bresenham(scene: QGraphicsScene, color: QColor, start: QPointF, end: QPointF):
-    if start == end:
-        plot_point(start)
-        return
-    
-    diff: QPointF = end - start
-    sx, sy = sign(diff.x()), sign(diff.y())
-    dx, dy = math.fabs(diff.x()), math.fabs(diff.y())
-    angle_tan = dy / dx
-    fl = 0
-
-    if angle_tan > 1:
-        dx, dy = dy, dx
-        angle_tan = 1 / angle_tan
-        fl = 1
-   
-    f = angle_tan - 0.5
-    x, y = start.x(), start.y()
-
-    for _ in range(1, int(dx) + 1):
-        plot_point(scene, color, QPointF(x, y))
-        if f > 0:
-            if fl == 1:
-                x += sx
-            else:
-                y += sy
-            f -= 1
-        elif f < 0:
-            if fl == 1:
-                y += sy
-            else:
-                x += sx
-            f += angle_tan
-        
-
-def int_bresenham(scene: QGraphicsScene, color: QColor, start: QPointF, end: QPointF):
-    if start == end:
-        plot_point(start)
-        return
-    
-    diff: QPointF = end - start
-    sx, sy = sign(diff.x()), sign(diff.y())
-    dx, dy = math.fabs(diff.x()), math.fabs(diff.y())
-    angle_tan = dy / dx
-    fl = 0
-
-    if angle_tan > 1:
-        dx, dy = dy, dx
-        angle_tan = 1 / angle_tan
-        fl = 1
-   
-    f = 2 * dy - dx
-    x, y = start.x(), start.y()
-
-    for _ in range(1, int(dx) + 1):
-        plot_point(scene, color, QPointF(x, y))
-        if f > 0:
-            if fl == 1:
-                x += sx
-            else:
-                y += sy
-            f -= 2 * dx
-        elif f < 0:
-            if fl == 1:
-                y += sy
-            else:
-                x += sx
-            f += 2 * dy
+    return rotated_vec.toPointF() + center
--- lab_03_01/src/interface.py
+++ lab_03_01/src/interface.py
@@ -1,9 +1,10 @@
-from PyQt6.QtWidgets import QMainWindow, QMenu, QMenuBar, QFileDialog, QMessageBox, \
+from PyQt6.QtWidgets import QMainWindow, QMessageBox, \
       QPushButton, QColorDialog, QLabel, QGraphicsView, QGraphicsScene, QTextEdit, QComboBox
-from PyQt6.QtGui import QAction, QColor, QTransform
-from PyQt6.QtCore import QPointF, QLineF
+from PyQt6.QtGui import QColor, QTransform
+from PyQt6.QtCore import QLineF
 from PyQt6 import uic
-import src.geometry as geo
+import src.plot_algoritms as plot
+import math
 
 class Interface(QMainWindow):
     def __init__(self):
@@ -16,25 +17,39 @@ class Interface(QMainWindow):
         transform.scale(1, -1)
         self.view.setTransform(transform)
 
-        self.background_color_button = self.findChild(QPushButton, "changeBackgroundColorBtn")
-        self.background_color_button.clicked.connect(self.choose_background_color)
+        self.plotter = plot.SegmentPlotter(self.scene)
+
         self.background_color_label = self.findChild(QLabel, "backgroundColor")
         self.background_color = QColor(255, 255, 255)
-
-        self.segment_color_button = self.findChild(QPushButton, "changeSegColorBtn")
-        self.segment_color_button.clicked.connect(self.choose_segment_color)
         self.segment_color_label = self.findChild(QLabel, "segmentColor")
         self.segment_color = QColor(0, 0, 0)
 
+        self.algorithm = self.findChild(QComboBox, "AlgorithmTypeBox")
+        self.__setup_buttons()
+        self.__setup_input_fields()
+
+    def __setup_input_fields(self):
         self.input_start_x = self.findChild(QTextEdit, "startCoordX")
         self.input_start_y = self.findChild(QTextEdit, "startCoordY")
         self.input_end_x = self.findChild(QTextEdit, "endCoordX")
         self.input_end_y = self.findChild(QTextEdit, "endCoordY")
+        self.input_angle = self.findChild(QTextEdit, "angleInput")
 
-        self.algorithm = self.findChild(QComboBox, "AlgorithmTypeBox")
-        
+    def __setup_buttons(self):
+        self.background_color_button = self.findChild(QPushButton, "changeBackgroundColorBtn")
+        self.background_color_button.clicked.connect(self.choose_background_color)
+
+        self.segment_color_button = self.findChild(QPushButton, "changeSegColorBtn")
+        self.segment_color_button.clicked.connect(self.choose_segment_color)
+    
         self.plot_segment_button = self.findChild(QPushButton, "plotSegmentBtn")
         self.plot_segment_button.clicked.connect(self.plot_segment)
+
+        self.plot_spectrum_button = self.findChild(QPushButton, "plotSpectrumBtn")
+        self.plot_spectrum_button.clicked.connect(self.plot_spectrum)
+
+        self.clear_button = self.findChild(QPushButton, "clearBtn")
+        self.clear_button.clicked.connect(self.scene.clear)
 
     def choose_background_color(self):
         color_dialog = QColorDialog(self)
@@ -53,31 +68,42 @@ class Interface(QMainWindow):
                 f"background-color: {self.segment_color.name()}"
             )
 
-    def get_points(self) -> tuple[QPointF, QPointF]:
+    def get_segment(self) -> tuple[QLineF, bool]:
         try:
             start_x = float(self.input_start_x.toPlainText())
             start_y = float(self.input_start_y.toPlainText())
             end_x = float(self.input_end_x.toPlainText())
             end_y = float(self.input_end_y.toPlainText())
         except ValueError:
-            return
+            QMessageBox.warning(self, "Ошибка", "Некорректные данные в полях ввода координат.")
+            return QLineF(), False
 
-        return QPointF(start_x, start_y), QPointF(end_x, end_y)
+        return QLineF(start_x, start_y, end_x, end_y), True
+    
+    def get_angle(self) -> tuple[float, bool]:
+        try:
+            angle = float(self.input_angle.toPlainText())
+        except ValueError:
+            QMessageBox.warning(self, "Ошибка", "Некорректные данные в поле ввода угла.")
+            return 0, False
+        
+        return math.radians(angle), True
 
     def plot_segment(self):
-        start, end = self.get_points()
-
-        match self.algorithm.currentIndex():
-            case 0:
-                geo.float_bresenham(self.scene, self.segment_color, start, end)
-
-            case 1:
-                geo.int_bresenham(self.scene, self.segment_color, start, end)
-
-            case 3:
-                geo.digital_differential_analyzer(self.scene, self.segment_color, start, end)
-
-            case 5: # "Алгоритм, использующий библиотечную функцию":
-                line = QLineF(start, end)
-                self.scene.addLine(line, self.segment_color)
-                
+        segment, ok = self.get_segment()
+        if not ok:
+            return
+        
+        algoritm = plot.Algorithm(self.algorithm.currentIndex())
+        self.plotter.plot(algoritm, segment, self.segment_color)
+        
+    def plot_spectrum(self):
+        segment, ok = self.get_segment()
+        if not ok:
+            return
+        angle, ok = self.get_angle()
+        if not ok:
+            return
+        
+        algoritm = plot.Algorithm(self.algorithm.currentIndex())
+        self.plotter.spectrum(algoritm, segment, self.segment_color, angle)
