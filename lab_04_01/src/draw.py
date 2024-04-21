444bb14d905b21421eff2124377aa678b3e520f0
hahaclassic <gavrilyukvlad221@gmail.com>
2024-04-21 20:03:44 +0300

    lab_04: the code is formatted

--- lab_04_01/src/draw.py
+++ lab_04_01/src/draw.py
@@ -48,9 +48,10 @@ class Plotter(ABC):
     @abstractmethod
     def plot(self, algo_type: Algorithm, circle: Circle, color: QColor) -> None:
         pass
-            
+
     @abstractmethod
-    def spectrum(self, algo_type: Algorithm, circle: Circle, spectrum: Spectrum, color: QColor) -> None:
+    def spectrum(self, algo_type: Algorithm, circle: Circle,
+                 spectrum: Spectrum, color: QColor) -> None:
         pass
 
     @abstractmethod
@@ -81,13 +82,12 @@ class CirclePlotter(Plotter):
     def plot(self, algo_type: Algorithm, circle: Circle, color: QColor) -> None:
         if algo_type not in self.algorithms or not isinstance(circle, Circle):
             return
-        
+
         if algo_type != Algorithm.BUILD_IN:
             points = self.algorithms[algo_type](circle)
             draw_points(self.scene, points, color)
         else:
             self.build_in(circle, color)
-            
 
     def spectrum(self, algo_type: Algorithm, circle: Circle, spectrum: Spectrum, color: QColor) -> None:
         if algo_type not in self.algorithms or not isinstance(circle, Circle):
@@ -97,15 +97,15 @@ class CirclePlotter(Plotter):
             self.plot(algo_type, circle, color)
             circle.radius += spectrum.step
 
-
     def canonical(self, circle: Circle) -> list[QPoint]:
         points: list[QPoint] = []
-        
+
         start = int(circle.center.x())
         end = start + int(circle.radius / math.sqrt(2)) + 1
 
         for x in range(start, end):
-            y = math.sqrt(circle.radius**2 - (x - circle.center.x())**2) + circle.center.y()
+            y = math.sqrt(circle.radius**2 -
+                          (x - circle.center.x())**2) + circle.center.y()
             add_symmetrical_points(points, QPointF(x, y), circle.center, True)
 
         return points
@@ -129,8 +129,8 @@ class CirclePlotter(Plotter):
         x, y = 0, circle.radius
         delta = 2 * (1 - circle.radius)
 
-        add_symmetrical_points(points, circle.center + QPointF(x, y), circle.center, True)
-
+        add_symmetrical_points(points, circle.center +
+                               QPointF(x, y), circle.center, True)
         while x < y:
             if delta <= 0:
                 delta_temp = 2 * (delta + y) - 1
@@ -149,25 +149,26 @@ class CirclePlotter(Plotter):
                     x += 1
                 else:
                     delta -= 2 * y - 1
-            add_symmetrical_points(points, circle.center + QPointF(x, y), circle.center, True)
+            add_symmetrical_points(
+                points, circle.center + QPointF(x, y), circle.center, True)
 
         return points
-
 
     def midpoint(self, circle: Circle) -> list[QPoint]:
         points: list[QPoint] = []
         x, y = circle.radius, 0
         delta = 1 - circle.radius
 
-        add_symmetrical_points(points, circle.center + QPointF(x, y), circle.center, True)
-
+        add_symmetrical_points(points, circle.center +
+                               QPointF(x, y), circle.center, True)
         while x > y:
             y += 1
             if delta > 0:
                 x -= 1
                 delta -= 2 * x - 2
             delta += 2 * y + 3
-            add_symmetrical_points(points, circle.center + QPointF(x, y), circle.center, True)
+            add_symmetrical_points(
+                points, circle.center + QPointF(x, y), circle.center, True)
 
         return points
 
@@ -175,7 +176,7 @@ class CirclePlotter(Plotter):
         self.scene.addEllipse(
             circle.center.x() - circle.radius,
             circle.center.y() - circle.radius,
-            circle.radius * 2, 
+            circle.radius * 2,
             circle.radius * 2,
             color
         )
@@ -188,14 +189,16 @@ class EllipsePlotter(Plotter):
     def plot(self, algo_type: Algorithm, ellipse: Ellipse, color: QColor) -> None:
         if algo_type not in self.algorithms or not isinstance(ellipse, Ellipse):
             return
-        
+
         if algo_type != Algorithm.BUILD_IN:
             points = self.algorithms[algo_type](ellipse)
             draw_points(self.scene, points, color)
         else:
             self.build_in(ellipse, color)
 
-    def spectrum(self, algo_type: Algorithm, ellipse: Ellipse, spectrum: Spectrum, color: QColor) -> None:
+    def spectrum(self, algo_type: Algorithm, ellipse: Ellipse,
+                 spectrum: Spectrum, color: QColor) -> None:
+
         if algo_type not in self.algorithms or not isinstance(ellipse, Ellipse):
             return
 
@@ -207,20 +210,23 @@ class EllipsePlotter(Plotter):
     def canonical(self, ellipse: Ellipse) -> list[QPoint]:
         points: list[QPoint] = []
         major, minor = ellipse.semi_major_axis, ellipse.semi_minor_axis
+        center_x, center_y = ellipse.center.x(), ellipse.center.y()
 
-        limit = int(ellipse.center.x() + major / math.sqrt(1 + minor**2 / major**2))
+        limit = int(center_x + major / math.sqrt(1 + minor**2 / major**2))
 
-        for x in range(int(ellipse.center.x()), limit + 1):
-            y = math.sqrt(major**2 * minor**2 \
-                - (x - ellipse.center.x())**2 * minor**2) / major + ellipse.center.y()
-            add_symmetrical_points(points, QPointF(x, y), ellipse.center, False)
-           
-        limit = int(ellipse.center.y() + minor / math.sqrt(1 + major**2 / minor**2))
+        for x in range(int(center_x), limit + 1):
+            y = math.sqrt(major**2 * minor**2 - (x - center_x) ** 2
+                          * minor**2) / major + center_y
+            add_symmetrical_points(points, QPointF(x, y),
+                                   ellipse.center, False)
 
-        for y in range(limit, int(ellipse.center.y()) - 1, -1):
-            x = math.sqrt(major**2 * minor**2 \
-                - (y - ellipse.center.y())**2 * major**2) / minor + ellipse.center.x()
-            add_symmetrical_points(points, QPointF(x, y), ellipse.center, False)
+        limit = int(center_y + minor / math.sqrt(1 + major**2 / minor**2))
+
+        for y in range(limit, int(center_y) - 1, -1):
+            x = math.sqrt(major**2 * minor**2 - (y - center_y) ** 2
+                          * major**2) / minor + center_x
+            add_symmetrical_points(points, QPointF(x, y),
+                                   ellipse.center, False)
 
         return points
 
@@ -234,10 +240,11 @@ class EllipsePlotter(Plotter):
         end = math.pi / 2 + step
         while t < end:
             x = ellipse.center.x() + ellipse.semi_major_axis * math.cos(t)
-            y = ellipse.center.y()+ ellipse.semi_minor_axis * math.sin(t)
+            y = ellipse.center.y() + ellipse.semi_minor_axis * math.sin(t)
             t += step
-            add_symmetrical_points(points, QPointF(x, y), ellipse.center, False)
-        
+            add_symmetrical_points(points, QPointF(x, y),
+                                   ellipse.center, False)
+
         return points
 
     def bresenham(self, ellipse: Ellipse) -> list[QPoint]:
@@ -246,7 +253,8 @@ class EllipsePlotter(Plotter):
         x, y = 0, minor
         delta = minor**2 - major**2 * (2 * minor + 1)
 
-        add_symmetrical_points(points, ellipse.center + QPointF(x, y), ellipse.center, False)
+        add_symmetrical_points(points, ellipse.center +
+                               QPointF(x, y), ellipse.center, False)
 
         while y > 0:
             if delta <= 0:
@@ -265,7 +273,8 @@ class EllipsePlotter(Plotter):
                     x += 1
                     delta += minor**2 * (2 * x + 1)
 
-            add_symmetrical_points(points, ellipse.center + QPointF(x, y), ellipse.center, False)
+            add_symmetrical_points(
+                points, ellipse.center + QPointF(x, y), ellipse.center, False)
 
         return points
 
@@ -278,7 +287,8 @@ class EllipsePlotter(Plotter):
         dx, dy = 2 * minor**2 * x, 2 * major**2 * y
 
         while dx < dy:
-            add_symmetrical_points(points, ellipse.center + QPointF(x, y), ellipse.center, False)
+            add_symmetrical_points(
+                points, ellipse.center + QPointF(x, y), ellipse.center, False)
             x += 1
             dx += 2 * minor**2
 
@@ -288,10 +298,12 @@ class EllipsePlotter(Plotter):
                 delta -= dy
             delta += dx + minor**2
 
-        delta = minor**2 * (x + 0.5)**2 + major**2 * (y - 1)**2 - major**2 * minor**2
+        delta = minor**2 * (x + 0.5)**2 + major**2 * \
+            (y - 1)**2 - major**2 * minor**2
 
         while y >= 0:
-            add_symmetrical_points(points, ellipse.center + QPointF(x, y), ellipse.center, False)
+            add_symmetrical_points(
+                points, ellipse.center + QPointF(x, y), ellipse.center, False)
             y -= 1
             dy -= 2 * major**2
 
@@ -308,18 +320,19 @@ class EllipsePlotter(Plotter):
             ellipse.center.x() - ellipse.semi_major_axis,
             ellipse.center.y() - ellipse.semi_minor_axis,
             ellipse.semi_major_axis * 2,
-            ellipse.semi_minor_axis * 2, 
+            ellipse.semi_minor_axis * 2,
             color
         )
 
-                                                                                          
+
 def draw_points(scene: QGraphicsScene, points: list[QPoint], color: QColor) -> None:
     for point in points:
         scene.addEllipse(point.x(), point.y(), 0.5, 0.5, color)
 
 
-def add_symmetrical_points(points: list[QPoint], point: QPointF|QPoint, 
-                           center: QPointF|QPoint, is_circle: bool):
+def add_symmetrical_points(points: list[QPoint], point: QPointF | QPoint,
+                           center: QPointF | QPoint, is_circle: bool):
+
     if isinstance(center, QPointF):
         center = center.toPoint()
     if isinstance(point, QPointF):
